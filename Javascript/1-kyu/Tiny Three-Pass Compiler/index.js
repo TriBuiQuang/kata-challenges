9b8140ff88fcf9bc6adb02b1230aff89206c371e
Tri Bui Quang <buiquangductri@gmail.com>
2022-11-19 21:55:12 +0700

    refactor

--- Javascript/1-kyu/Tiny Three-Pass Compiler/index.js
+++ Javascript/1-kyu/Tiny Three-Pass Compiler/index.js
@@ -1,9 +1,7 @@
-const operators = {
-   "+": true,
-   "-": true,
-   "*": true,
-   "/": true,
-};
+const innVal = "imm";
+const argVal = "arg";
+
+const operators = ["+", "-", "*", "/"];
 
 const operations = {
    "+": (a, b) => a + b,
@@ -12,171 +10,161 @@ const operations = {
    "/": (a, b) => a / b,
 };
 
-function samePrecedence(operator1, operator2) {
-   return (
-      operator1 === operator2 ||
-      (operator1 === "*" && operator2 === "/") ||
-      (operator1 === "/" && operator2 === "*") ||
-      (operator1 === "+" && operator2 === "-") ||
-      (operator1 === "-" && operator2 === "+")
-   );
-}
+const isHigherOrSame = (firstOp, secondOp) => {
+   const opAdSub = operators.slice(0, 2);
+   const opMultiDiv = operators.slice(2, 4);
 
-function higherPrecedence(operator1, operator2) {
-   return (operator1 === "*" || operator1 === "/") && (operator2 === "+" || operator2 === "-");
-}
+   const same =
+      firstOp === secondOp ||
+      (opMultiDiv.includes(firstOp) && opMultiDiv.includes(secondOp)) ||
+      (opAdSub.includes(firstOp) && opAdSub.includes(secondOp));
 
-function higherOrSamePresendence(operator1, operator2) {
-   return samePrecedence(operator1, operator2) || higherPrecedence(operator1, operator2);
-}
+   const higher = opMultiDiv.includes(firstOp) && opAdSub.includes(secondOp);
+
+   return same || higher;
+};
 
 class Compiler {
-   compile(program) {
-      return this.pass3(this.pass2(this.pass1(program)));
-   }
+   compile = (program) => this.pass3(this.pass2(this.pass1(program)));
 
-   tokenize(program) {
-      // Turn a program string into an array of tokens.  Each token
-      // is either '[', ']', '(', ')', '+', '-', '*', '/', a variable
-      // name or a number (as a string)
-      // eslint-disable-next-line
-      var regex = /\s*([-+*/\(\)\[\]]|[A-Za-z]+|[0-9]+)\s*/g;
-      // eslint-disable-next-line
+   tokenize = (program) => {
+      const regex = /\s*([-+*/\(\)\[\]]|[A-Za-z]+|[0-9]+)\s*/g;
+
       return program
          .replace(regex, ":$1")
          .substring(1)
          .split(":")
-         .map(function (tok) {
-            // eslint-disable-next-line
-            return isNaN(tok) ? tok : tok | 0;
-         });
-   }
+         .map((token) => (isNaN(token) ? token : token | 0));
+   };
 
-   tokensToAST(args, tokens) {
+   tokensToAST = (args, tokens) => {
       const outputStack = [];
       const operatorStack = [];
+
       const pushOperation = () => {
-         // TODO: error checking
-         const op = operatorStack.shift();
-         const b = outputStack.pop();
-         const a = outputStack.pop();
-         outputStack.push({
-            op,
-            a,
-            b,
-         });
+         const operator = operatorStack.shift();
+         const outputB = outputStack.pop();
+         const outputA = outputStack.pop();
+         outputStack.push({ op: operator, a: outputA, b: outputB });
       };
-      for (let i = 0; i < tokens.length; i += 1) {
+
+      for (let i = 0; i < tokens.length; i++) {
          const token = tokens[i];
-         if (!(token in operators) && token !== "(" && token !== ")") {
+
+         if (!operators.includes(token) && token !== "(" && token !== ")") {
             if (!isNaN(token)) {
-               outputStack.push({
-                  op: "imm",
-                  n: token,
-               });
-            } else {
-               // TODO: error checking
-               outputStack.push({
-                  op: "arg",
-                  n: args[token],
-               });
+               outputStack.push({ op: innVal, n: token });
+               continue;
             }
-         } else if (token in operators) {
-            while (operatorStack.length && higherOrSamePresendence(operatorStack[0], token)) {
+
+            outputStack.push({ op: argVal, n: args[token] });
+            continue;
+         }
+
+         if (operators.includes(token) || token === "(") {
+            while (operatorStack.length && isHigherOrSame(operatorStack[0], token)) {
                pushOperation();
             }
+
             operatorStack.unshift(token);
-         } else if (token === "(") {
-            operatorStack.unshift(token);
-         } else if (token === ")") {
+            continue;
+         }
+
+         if (token === ")") {
             while (operatorStack.length && operatorStack[0] !== "(") {
                pushOperation();
             }
+
             if (operatorStack[0] === "(") {
                operatorStack.shift();
             }
          }
       }
+
       while (operatorStack.length) {
          pushOperation();
       }
       return outputStack.pop();
-   }
+   };
 
-   parseArguments(tokens) {
+   parseArguments = (tokens) => {
       const errorMessage = "Invalid argument list.";
-      const args = {};
+      const obj = {};
       let argCount = 0;
-      for (let i = 0; i < tokens.length; i += 1) {
-         const token = tokens[i];
-         if (i === 0 && token !== "[") {
-            throw new Error(errorMessage);
-         } else if (i > 0) {
-            if (token === "]") {
-               return {
-                  args,
-                  i,
-               };
-            }
-            args[token] = argCount;
-            argCount += 1;
-         }
-      }
-      throw new Error(errorMessage);
-   }
 
-   pass1(program) {
+      if (tokens[0] !== "[") throw new Error(errorMessage);
+
+      for (let i = 1; i < tokens.length; i += 1) {
+         const token = tokens[i];
+
+         if (token === "]") {
+            return { args: obj, i };
+         }
+
+         obj[token] = argCount;
+         argCount += 1;
+      }
+
+      throw new Error(errorMessage);
+   };
+
+   pass1 = (program) => {
       const tokens = this.tokenize(program);
       const { args, i } = this.parseArguments(tokens);
       return this.tokensToAST(args, tokens.slice(i + 1));
-   }
+   };
 
-   pass2(ast) {
-      function performOperation(node) {
-         if (operators[node.op]) {
-            if (node.a.op !== "imm") {
-               // eslint-disable-next-line
-               node.a = performOperation(node.a);
-            }
-            if (node.b.op !== "imm") {
-               // eslint-disable-next-line
-               node.b = performOperation(node.b);
-            }
-            if (node.a.op === "imm" && node.b.op === "imm") {
-               return {
-                  op: "imm",
-                  n: operations[node.op](node.a.n, node.b.n),
-               };
-            }
+   pass2 = (ast) => {
+      const performOp = (node) => {
+         if (!operators.includes(node.op)) return node;
+
+         if (node.a.op !== innVal) {
+            node.a = performOp(node.a);
          }
+
+         if (node.b.op !== innVal) {
+            node.b = performOp(node.b);
+         }
+
+         if (node.a.op === innVal && node.b.op === innVal) {
+            return {
+               op: innVal,
+               n: operations[node.op](node.a.n, node.b.n),
+            };
+         }
+
          return node;
-      }
-      return performOperation(ast);
-   }
-
-   pass3(ast) {
-      const operationInstructions = {
-         "+": "AD",
-         "-": "SU",
-         "*": "MU",
-         "/": "DI",
       };
-      function generateCode(node) {
-         if (operationInstructions[node.op]) {
-            return [...generateCode(node.a), "PU", ...generateCode(node.b), "SW", "PO", operationInstructions[node.op]];
-         }
+      return performOp(ast);
+   };
 
+   pass3 = (ast) => {
+      const genCode = (node) => {
          const instructions = [];
-         if (node.op === "imm") {
-            instructions.unshift(`IM ${node.n}`);
-         } else if (node.op === "arg") {
-            instructions.unshift(`AR ${node.n}`);
+         let valRemove = "";
+         const operateInstructions = {
+            "+": "AD",
+            "-": "SU",
+            "*": "MU",
+            "/": "DI",
+         };
+
+         if (operateInstructions[node.op])
+            return [...genCode(node.a), "PU", ...genCode(node.b), "SW", "PO", operateInstructions[node.op]];
+
+         switch (node.op) {
+            case innVal:
+               valRemove = `IM ${node.n}`;
+               break;
+            case argVal:
+               valRemove = `AR ${node.n}`;
+               break;
          }
+
+         instructions.unshift(valRemove);
          return instructions;
-      }
+      };
 
-      return generateCode(ast);
-   }
+      return genCode(ast);
+   };
 }
-
-module.exports = Compiler;
